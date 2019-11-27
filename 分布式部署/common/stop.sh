021cd80b22bc756e6ed010047c9ab0bdd5d13469
zhuchunzhan <zhuchunzhan@163.com>
2019-11-27 18:59:03 +0800

    canal

--- 分布式部署/common/stop.sh
+++ 分布式部署/common/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export customer_code=${PWD##*/}  #默认当前父目录作为客户code
-export dubboadmin_port=9000
+export dubboadmin_port=9014
 docker-compose stop
 echo "The zookeeper,redis and dubboadmin has been stoped."
