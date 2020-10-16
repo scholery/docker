0842838087de4f209e852170b80b65340a017268
scholery <scholery@gmail.com>
2020-10-16 13:46:30 +0800

    备份视图

--- 分布式部署/data/mysqlbackup.sh
+++ 分布式部署/data/mysqlbackup.sh
@@ -1,2 +1,2 @@
-mysqldump -h127.0.0.1 -uroot -pLonger.1234 --default-character-set=utf8  --databases lfcp_jsnd_web> /home/lfcp/backup/mysql/lfcp_jsnd_web_$(date +%Y%m%d_%H%M%S).sql
+mysqldump -h127.0.0.1 -uroot -pLonger.1234 --default-character-set=utf8  --skip-opt --skip-comments --complete-insert --databases lfcp_jsnd_web> /home/lfcp/backup/mysql/lfcp_jsnd_web_$(date +%Y%m%d_%H%M%S).sql
 find /home/lfcp/backup/mysql -mtime +30 -type f | xargs rm -f
